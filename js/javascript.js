600f50894610f3722efb10fdd3a51c9a3ab3d495
adimnych <andydimnych@gmail.com>
2017-10-25 02:56:34 -0500

    Updated the highlighting as well as the next function
    Java script code added finally

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,31 @@
 #this is where i will put my functions
+/*this is for when they click the submit button*/
+function button() {
+	alert("GREAT JAZZ PLAYLISTS ON THE WAY!");
+	return true;
+}
+/*this is when the click in the typing mode*/
+
+function highlight(clicker){
+	clicker.style.background = "blue";
+}
+
+/*this is to return then given in all lowercase letters */
+function next() {
+	var fnam= document.getElementById("first-name");
+	first-name.value = first-name.value.toLowerCase();
+
+	var lnam= document.getElementById("last-name");
+	last-name.value = last-name.value.toLowerCase();
+
+	var adr= document.getElementById("email");
+	email.value = email.value.toLowerCase();
+
+	var user-name= document.getElementById("user-name");
+	user-name.value = user-name.value.toLowerCase();
+
+	var password= document.getElementById("password");
+	password.value = password.value.toLowerCase();
+}
+
+
