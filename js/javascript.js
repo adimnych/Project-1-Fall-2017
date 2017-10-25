4032b9a53fb07d96f47c62e49526117fbcc262e0
adimnych <andydimnych@gmail.com>
2017-10-25 03:10:51 -0500

    Updated the locations of the picsa
    also change indentation for javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,28 @@
 /*this is for when they click the submit button*/
 function button() {
-	alert("GREAT JAZZ PLAYLISTS ON THE WAY!");
-	return true;
+alert("GREAT JAZZ PLAYLISTS ON THE WAY!");
+return true;
 }
 /*this is when the click in the typing mode*/
-
 function highlight(clicker){
-	clicker.style.background = "blue";
+clicker.style.background = "blue";
 }
-
 /*this is to return then given in all lowercase letters */
 function next() {
-	var fnam= document.getElementById("first-name");
-	first-name.value = first-name.value.toLowerCase();
+var fnam= document.getElementById("first-name");
+first-name.value = first-name.value.toLowerCase();
 
-	var lnam= document.getElementById("last-name");
-	last-name.value = last-name.value.toLowerCase();
+var lnam= document.getElementById("last-name");
+last-name.value = last-name.value.toLowerCase();
 
-	var adr= document.getElementById("email");
-	email.value = email.value.toLowerCase();
+var adr= document.getElementById("email");
+email.value = email.value.toLowerCase();
 
-	var user-name= document.getElementById("user-name");
-	user-name.value = user-name.value.toLowerCase();
+var user-name= document.getElementById("user-name");
+user-name.value = user-name.value.toLowerCase();
 
-	var password= document.getElementById("password");
-	password.value = password.value.toLowerCase();
+var password= document.getElementById("password");
+password.value = password.value.toLowerCase();
 }
 
 
